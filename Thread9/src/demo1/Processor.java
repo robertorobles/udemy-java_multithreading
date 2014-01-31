c0ec2d1965be5bf8d564e121b8351775a7771986
robertorobles <info@robertorobles.es>
2014-01-31 23:35:53 +0100

    Part 9 - A Worked Example Using Low-Level Synchronization

---
+++ Thread9/src/demo1/Processor.java
@@ -0,0 +1,52 @@
+package demo1;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Processor {
+
+	private LinkedList<Integer> list = new LinkedList<Integer>();
+	private final int LIMIT = 10;
+	private Object lock = new Object();
+
+	public void produce() throws InterruptedException {
+
+		int value = 0;
+
+		while (true) {
+
+			synchronized (lock) {
+
+				while (list.size() == LIMIT) {
+					lock.wait();
+				}
+
+				list.add(value++);
+				lock.notify();
+			}
+
+		}
+	}
+
+	public void consume() throws InterruptedException {
+
+		Random random = new Random();
+
+		while (true) {
+
+			synchronized (lock) {
+
+				while (list.size() == 0) {
+					lock.wait();
+				}
+
+				System.out.print("List size is: " + list.size());
+				int value = list.removeFirst();
+				System.out.println("; value is: " + value);
+				lock.notify();
+			}
+
+			Thread.sleep(random.nextInt(1000));
+		}
+	}
+}
