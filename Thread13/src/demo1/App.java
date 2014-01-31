235c9706c342172a179b81f821d3e2a4f4b01954
robertorobles <info@robertorobles.es>
2014-01-31 23:50:24 +0100

    Part 13 - Callable and Future

---
+++ Thread13/src/demo1/App.java
@@ -0,0 +1,55 @@
+package demo1;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class App {
+
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		Future<Integer> future = executor.submit(new Callable<Integer>() {
+
+			@Override
+			public Integer call() throws Exception {
+				Random random = new Random();
+				int duration = random.nextInt(4000);
+
+				if (duration > 2000) {
+					throw new IOException("Sleeping for too long.");
+				}
+
+				System.out.println("Starting ...");
+
+				try {
+					Thread.sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("Finished.");
+
+				return duration;
+			}
+
+		});
+
+		executor.shutdown();
+
+		try {
+			System.out.println("Result is: " + future.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			IOException ex = (IOException) e.getCause();
+
+			System.out.println(ex.getMessage());
+		}
+	}
+
+}
